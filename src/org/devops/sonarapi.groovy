3324ff9cb2ca54fd8149fd6f410337f4d90b51de
Nice <57763864+nice0111@users.noreply.github.com>
2021-09-01 16:36:29 +0800

    Update sonarapi.groovy

--- src/org/devops/sonarapi.groovy
+++ src/org/devops/sonarapi.groovy
@@ -1,1 +1,17 @@
 
+//封装HTTP
+
+def HttpReq(reqType,reqUrl,reqBody){
+    def sonarServer = "http://192.168.1.200:30090/api"
+   
+    result = httpRequest authentication: 'sonar-admin-user',
+            httpMode: reqType, 
+            contentType: "APPLICATION_JSON",
+            consoleLogResponseBody: true,
+            ignoreSslErrors: true, 
+            requestBody: reqBody,
+            url: "${sonarServer}/${reqUrl}"
+            //quiet: true
+    
+    return result
+}
