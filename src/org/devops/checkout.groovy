1bbbc5211888c09d029892627a7598f80c06b239
Nice <57763864+nice0111@users.noreply.github.com>
2021-11-02 10:00:31 +0800

    Update checkout.groovy

--- src/org/devops/checkout.groovy
+++ src/org/devops/checkout.groovy
@@ -7,7 +7,7 @@ def checkout(branch, giturl ) {
             [
             $class: 'GitSCM', branches: [[name: '${branch}']], 
             extensions: [], 
-            userRemoteConfigs: [[credentialsId: 'devin-001', 
+            userRemoteConfigs: [[credentialsId: 'f6d8237b-e3d9-4d75-a2c1-15a6578ed785', 
             url: '${giturl}']]
             ]
   )
